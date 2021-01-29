f855da8ab9ca094c67b09284666e387a738a0243
krishnendu2007 <74901025+krishnendu2007@users.noreply.github.com>
2021-01-29 22:01:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
+var dustbingreenImg;
+var paperImg;
+
+function preload() {
+	dustbingreenImg = loadImage("dustbingreen.png")
+    paperImg = loadImage("paper.png")
+
+
+}
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+ 
+
+  groundObject.display();
+  dustbinObj.display();
+
+}
+
